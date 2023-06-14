fd6405b5c862749229ad3ef90378b797a2f6d6c9
Alice Hannecart <ahannecart@hotmail.com>
2023-06-14 12:19:25 +0200

    new version streamlit wednesday

--- app.py
+++ app.py
@@ -8,30 +8,31 @@ import io
 import json
 import openai
 import time
+from pathlib import Path
+import base64
 
 #load variables
 BASE_URI = st.secrets['cloud_api_uri']
 url = BASE_URI + '/predict'
 OPENAI_API_KEY=st.secrets['OPENAI_API_KEY']
 
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
 
-green_background = """
-<style>
-body {
-    background-color: lightgreen;
-}
-</style>
-"""
 def validate_result(api_result,threshold=70):
     '''
     This function ensure a minimal score and print result
     '''
     first_category = list(api_result.keys())[0]
     first_value_accuracy = round(list(api_result.values())[0]*100,2)
+    if first_category == 'Background without leaves':
+        return "I'm sorry, I'm not able to recognize the leaf, could you feed me with another image please?"
     if first_value_accuracy > threshold:
-         return "ok - the model is at "+str(first_value_accuracy) + "% confident that is a '" + first_category + "' disease"
+         return "Yes! I'm fairly sure, at "+ str(first_value_accuracy) + "% that I've detected a " + first_category + "!"
     else:
-         return "KO - Accuracy <  " + threshold
+         return "MmmmmmH... I'm not quite confident, could we try again with another image please?"
 
 def chat_with_chatgpt(prompt, model="text-davinci-003"):
     response = openai.Completion.create(
@@ -54,12 +55,12 @@ def loading_message():
     message_list = [
             'Setting up the API url to:  '+url,
             'The image is sent to API...',
-            'The image is rescaled to 258x258 pixels...',
+            'The image is rescaled to 256x256 pixels...',
             'The image is being rotated randomly to create augmented features...',
             'The image is now ready to be analyzed...',
-            'The system is now checking if prediction have sense...',
+            'Guillaume is finetuning the model :)',
+            'Raphaël is checking if the prediction makes sense...',
             'Alice is looking into a dictionary about the detected disease...',
-            'Guillaume is doing his finetuning the model :)',
             'Getting back the 3 mains probabilities from the API',
             'Be patient, it \' coming.... :)',
             'DONE !'
@@ -73,8 +74,8 @@ def loading_message():
         my_bar.progress(percent_complete + 1, text=message_list[message_id])
     return None
 
-# Afficher le fond vert pâle à l'aide de st.markdown()
-st.markdown(green_background, unsafe_allow_html=True)
+# #Afficher le fond vert pâle à l'aide de st.markdown()
+# st.markdown(green_background, unsafe_allow_html=True)
 
 left_column, right_column = st.columns(2)
 
@@ -82,12 +83,25 @@ left_column, right_column = st.columns(2)
 #with left_column:
 
 # Titre en vert
-st.title('LeafScan')
-st.markdown('<style>h1{color: green;}</style>', unsafe_allow_html=True)
+header_html = f"""
+            <h1 style='text-align: center; text-color:#081C15'>
+                LeafScan App
+                <img
+                    src='data:image/png;base64,{
+                        img_to_bytes(
+                            "media/icons/001-leaf.png"
+                            )}'
+                    class='img-fluid'>
+            </h1>
+                """
+st.markdown(
+    header_html, unsafe_allow_html=True,
+)
+#st.markdown('<style>h1{color: #2D6A4F;}</style>', unsafe_allow_html=True)
 # Section Drag and Drop
 st.header('Drag and Drop')
 
-uploaded_files = st.file_uploader("Choose image files", accept_multiple_files=True, type="jpg")
+uploaded_files = st.file_uploader("Choose image files", accept_multiple_files=True, type=None)
 ask_chatGPT = st.checkbox("I'd like to receive chatGPT advices to treat the detected disease (if any)")
 
 # Côté droit (Prédictions et images)
@@ -101,10 +115,12 @@ if uploaded_files is not None:
            i=0
 
            image = Image.open(upld)
-           st.image(image, caption='Uploaded Image.', width=200)
+           st.image(image, caption=f'Uploaded Image ({image.format}).', width=200)
+           if image.format == "PNG":
+            image = image.convert("RGB")
            # convert the PIL image to byte array
            image_bytes = io.BytesIO()
-           image.save(image_bytes, format='JPEG')
+           image.save(image_bytes, format="JPEG")
            image_bytes = image_bytes.getvalue()
 
            # Use 'rb' if you get an error about 'bytes-like object is required, not str'
